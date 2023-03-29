92fc7812fc762714c9b7f12f25468a2d0f9ce59f
Abdulrahman Badr <41681016+abdulrahman9901@users.noreply.github.com>
2023-03-29 04:07:04 +0200

    Update app.js

--- app.js
+++ app.js
@@ -126,7 +126,7 @@ if(currentAuthor== "Source Title")
   document.getElementById('tweet-quote').setAttribute(
     'href',
     'https://twitter.com/intent/tweet?hashtags=BQG&text=' +
-      encodeURIComponent(' " ' + currentQuote + ' " '+ "%25250A" + currentAuthor)
+      encodeURIComponent(' " ' + currentQuote + ' " '+ '\n' + currentAuthor)
   );
 };
 
